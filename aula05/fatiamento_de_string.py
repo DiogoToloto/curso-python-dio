d8127a35aef30e067a56b82710e8f9547c2c9db5
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-03-28 05:26:57 -0300

    fatiamento de string

---
+++ aula05/fatiamento_de_string.py
@@ -0,0 +1,10 @@
+nome = "Diogo Gabriel Toloto da Silva"
+
+print(nome[0])
+print(nome[-1])
+print(nome[:13])
+print(nome[13:])
+print(nome[13:20])
+print(nome[13:20:2])
+print(nome[:])
+print(nome[::-1])
